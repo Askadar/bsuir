ef32e232ae826bb9ba8917f0291e9a30bf21ae92
Azriel <askadar.ru@gmail.com>
2016-10-11 13:17:02 +0300

    Finalized lw2, RC1

--- networks/lw2/client.js
+++ networks/lw2/client.js
@@ -4,6 +4,18 @@ const readline = require('readline');
 //deafult address
 const port = 8080;
 const host = 'localhost';
+//menu text
+function menuText(data){
+	const menu = `[Show] - output list of details; 
+[Add](detail name, detail count, manufacturer ID) - add a detail to list, returns detail id on success; 
+[Edit](detail name or id) - edit detail in list found by id or name; 
+[Delete](detail id or name) - delete detail from list
+[(detail name)] - output selected detail data
+[Exit] - end connection and exit
+`;
+	const input = '\nInput: '
+	return data ? menu+data+input : menu+input;
+}
 //create interfaces for readlne to work
 const rl = readline.createInterface({
 	input: process.stdin,
@@ -14,7 +26,52 @@ const client = new net.Socket();
 
 client.on('data', (data)=>{
 	//console.log('Client received: ', data);
-	console.log('Response: ', data.toString());
+	//console.log('Response: ', data.toString());
+	let action =JSON.parse(data.toString());
+	switch (action.type){
+		case 'show':
+			menu(action.response+(action.data.length > 0 ? action.data.map(a=>a.name).join(', ') : 'none'));
+		break;
+		case 'add':
+			menu(action.response);
+		break;
+		case 'edit':
+			if(action.response != 'success')
+				rl.question(action.response, answer=>{
+					client.write(JSON.stringify({type:'edit', data: answer.split(' ')}))
+				})
+			else
+				menu('Successful edit');
+		break;
+		case 'delete':
+			if(action.response != 'success')
+				rl.question(action.response, answer=>{
+					if (answer.toLowerCase() != 'cancel')
+						client.write(JSON.stringify({type:'delete', data: [answer,'confirm']}))
+					else
+						menu('Delete canceled')
+				})
+			else
+				menu('Successful delete');      
+		break;
+		case 'stuff':
+			var info = '\n';
+			console.log(action);
+			action.data.map(row => {
+				var textRow = ''
+				Object.keys(row).map(k => {
+					textRow += k+' - '+row[k]+ (k != 'manufacturer' ? ', ' :'');
+					//console.log('first tr', textRow, row, k, row[k]);
+				})
+				//console.log('second tr', textRow, info);
+				info += textRow+'\n';
+			})
+			console.log(info);
+			menu(action.response+info.slice(0,-1))
+		break;
+		default:
+			menu('Server responded with corrupted action')
+	}
 })
 
 client.on('close', ()=>{
@@ -30,28 +87,36 @@ client.connect(port, host, ()=>{
 	console.log(`Connected to ${host}:${port}`); //report about succesfull connection
 	menu();
 })
-function menu(){
-	rl.question(`[Show] - output list of details; 
-		[Add](detail name, detail count, manufacturer ID) - add a detail to list, returns detail id on success; 
-		[Edit](detail name or id) - edit detail in list found by id or name; 
-		[Delete](detail id or name) - delete detail from list
-		[(detail name)] - output selected detail data`, (answer) => {
+function menu(data){
+	//print out menu
+	rl.question(menuText(data), (answer) => {
 			switch(answer.toLowerCase()){
 				case 'show':
-					client.write('show');
+					client.write(JSON.stringify({type:'show'}));
 				break;
 				case 'add':
-					
+					rl.question('Enter detail name, quantity and manufacturer ID [delimited by space]: ', (answer) => {
+						client.write(JSON.stringify({type:'add', data: answer.split(' ')}));
+					})
 				break;
-
+				case 'edit':        
+					rl.question('Enter detail name to edit: ', (answer) => {
+						client.write(JSON.stringify({type:'edit', data: answer.split(' ')}));
+					})
+					//client.write(JSON.stringify({type:'edit'}));
+				break;
+				case 'delete':
+					rl.question('Enter detail name to delete: ', (answer) => {
+						client.write(JSON.stringify({type:'delete', data: answer.split(' ')}));
+					})
+				break;
+				case 'exit':
+					rl.close()
+					client.end();
+				break;
+				default:
+					client.write(JSON.stringify({type:'stuff', data: answer}));
 			}
-			var requestString = JSON.stringify(answer.replace(/[^0-9.,]+/g, '|').split('|').slice(0,2)); //  stringify array that we made from user input by removing everything but numbers and '.' ',' symbols and then splitting string by previously inserted token and slicing everything but first two numbers
-				client.write(requestString);
-			console.log('Sent to server: ', requestString);
 		}
 	);
-}
-function exit(){
-	rl.close();
-	client.end();
 }
--- networks/lw2/server.js
+++ networks/lw2/server.js
@@ -4,55 +4,93 @@ const net = require('net');
 const port = 8080;
 //creating server with anonymous callback for new connections
 const server = net.createServer(socket => {
-    console.log('Connection accepted');
+	console.log('Connection accepted');
 	//handler for proper connection end
-    socket.on('end', () => {
-        console.log('Connection closed');
-    })
+	socket.on('end', () => {
+		console.log('Connection closed');
+	})
 	//handler for errors and lost connections
-    socket.on('error', (err) => {
-        if (err.code == 'ECONNRESET')
-            console.log('TCP connection ended abruptly');
-        else
-            console.log(err);
-        //throw err;
-    })
+	socket.on('error', (err) => {
+		if (err.code == 'ECONNRESET')
+			console.log('TCP connection ended abruptly');
+		else
+			console.log(err);
+		//throw err;
+	})
 	// handler for received data
-    socket.on('data', data => {
-            //console.log('Received data from client: ', data);
-            console.log('Received data from client(stringified in UTF-8): ', data.toString()); //logging received data translated from Buffer object to simple utf-8 string, we're hoping for JSONed array with 2 numbers
-			try{
-	            var parsedData = JSON.parse(data.toString());
-			}
-			catch (err){
-				console.log(`Data is corruptedd or isn't JSON {${err}}`);
-				socket.write('Wrong input, waiting for JSONed array');
-				return;
-			}
-            parsedData = parsedData.map(a => parseFloat(a)); //converting 2 strings to numbers
-            console.log('Calculating');
-            if (!isNaN(parsedData[0]) && !isNaN(parsedData[1])) { //check if 2 values are valid numbers and
-                socket.write('Wait for your data to be calculated');
-				// send client confirmation that his data is accepted and simulate calculations time
-                setTimeout(() => {
-					// calculate gcd
-                    let a = parsedData[0],
-                        b = parsedData[1];
-                    while (a != 0 && b != 0) {
-                        a > b ? a %= b : b %= a
-                    };
-                    socket.write('Your GCD: ' + (a + b));
-                }, (Math.random() + 0.2) * 5000) //random time between 1 to 6 seconds, in ms
-            } else {
-                socket.write('Wrong input') //in case input isn't adequate deny client in calculating
-            }
-
-        })
-        //socket.write('Hello from server');
-        //console.log('dev',socket.pipe);
-        //socket.pipe(socket);
+	socket.on('data', data => {
+		//console.log('Received data from client: ', data);
+		console.log('Request: ', data.toString()); //logging received data translated from Buffer object to simple utf-8 string, we're hoping for JSONed array with 2 numbers
+		try{
+			var parsedData = JSON.parse(data.toString());
+		}
+		catch (err){
+			console.log(`Data is corruptedd or isn't JSON {${err}}`);
+			socket.write('Wrong input, waiting for JSONed action (Action {type:\'x\', data:{}})');
+			return;
+		}
+		let response = reducer(parsedData);
+		console.log(response);
+		socket.write(response);
+		
+	})
+	//socket.write('Hello from server');
+	//console.log('dev',socket.pipe);
+	//socket.pipe(socket);
 })
 server.listen(port, () => {
 	// onlisten event handler - log succesfull port binding
-    console.log(`Server started listening on port ${port}. Hello from listenig callback`);
+	console.log(`Server started listening on port ${port}. Hello from listenig callback`);
 })
+
+//'reducer' function, global state for now
+var state = [{id:0, name:'Nail', quantity:1978, manufacturer: 693},
+			{id:1, name:'Screw', quantity:1703, manufacturer: 693},
+			{id:2, name:'Plug', quantity:126, manufacturer: 446},
+			{id:5, name:'Nail', quantity:612, manufacturer: 335},
+			{id:6, name:'Hinge', quantity:1989, manufacturer: 446},
+			{id:7, name:'Screw', quantity:2387, manufacturer: 446},
+			{id:8, name:'Oil', quantity:98000, manufacturer: 693},
+			{id:11, name:'Straw', quantity:78490, manufacturer: 335}]
+function reducer(action){
+	switch (action.type){
+		case 'show':
+			return JSON.stringify({response:'Details: ',type: 'show', data: state});
+		break;
+		case 'add':
+			state.push({id:state[state.length-1].id+1, name: action.data[0], quantity: action.data[1], manufacturer:  action.data[2]})
+			return JSON.stringify({response:'Succesfuly added '+action.data.name, type: 'add'})
+		break;
+		case 'edit':
+			if (action.data.length < 2){
+				let foundRows = state.filter(a => a.id == action.data[0] || a.name == action.data[0]).map(row => 'manufacturer: '+row.manufacturer+' | '+row.id+' - '+row.quantity).join('\n');
+				let completeResponse = foundRows.length > 1 ? 'Found this material(s):\n'+foundRows+'\n[enter id and new quantity to edit]:' : 'No materials found';
+				return JSON.stringify({response: completeResponse, type:'edit', data:foundRows})
+			}
+			else {
+				let id;
+				state.map((a,b) => { if(a.id == action.data[0]) id = b;});
+				state[id].quantity = action.data[1];
+				return JSON.stringify({type: 'edit', response: 'success', })
+			}
+		break;
+		case 'delete':
+			if (typeof action.data[0] != 'number' && action.data[1] != 'confirm'){
+				let foundRows = state.filter(a => a.id == action.data[0] || a.name == action.data[0]).map(row => 'manufacturer: '+row.manufacturer+' | '+row.id+' - '+row.quantity).join('\n');
+				let completeResponse = foundRows.length > 1 ? 'Found this material(s):\n'+foundRows+'\n[enter id and new quantity to edit]:' : 'No materials found';
+				return JSON.stringify({response: completeResponse, type:'delete', data:foundRows})
+			}
+			else {
+				let id;
+				state.map((a,b) => { if(a.id == action.data[0]) id = b;});
+				state.splice(id,1);
+				return JSON.stringify({type: 'delete', response: 'success', })
+			}
+		break;
+		case 'stuff':
+			return JSON.stringify({response:'Info about detail(s): ', data: state.filter(a => a.name == action.data || a.id == action.data), type: 'stuff'})
+		break;
+		default:
+			return Object.assign(action, {type: 'error', response: 'wrong action type provided'})
+	}
+}
